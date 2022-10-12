a25dc6bbb23ea46d9fe6c148af1a85f39a7cbbaa
yuda <yudajgim@gmail.com>
2022-10-12 17:25:28 +0900

    [초기 설정] 프로젝트 생성 및 git 초기화

---
+++ src/main/java/com/laundry_m/mvc/laundry_m/App.java
@@ -0,0 +1,13 @@
+package com.laundry_m.mvc.laundry_m;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
