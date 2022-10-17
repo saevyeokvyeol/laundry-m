051098bde61daea3432eccc930af69c1634ff1e7
yuda <yudajgim@gmail.com>
2022-10-17 10:08:55 +0900

    [파일 추가] Duplication, NotExist, NotLogin Exception 추가

---
+++ src/main/java/com/laundry_m/mvc/laundry_m/exception/NotExistException.java
@@ -0,0 +1,9 @@
+package com.laundry_m.mvc.laundry_m.exception;
+
+public class NotExistException extends Exception {
+	public NotExistException() {}
+
+	public NotExistException(String message) {
+		super(message);
+	}
+}
