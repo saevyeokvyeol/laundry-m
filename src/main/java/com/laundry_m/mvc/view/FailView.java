642fd835121ec5c8c6754372a684ed5d3111c10e
cabzs <sunj20037@gmail.com>
2022-10-21 12:29:38 +0900

    [파일 추가] failView, successView 추가

---
+++ src/main/java/com/laundry_m/mvc/view/FailView.java
@@ -0,0 +1,9 @@
+package com.laundry_m.mvc.view;
+
+public class FailView {
+	
+	public static void errorMessage(String message) {
+		System.out.println(message);
+	}
+
+}
---
+++ src/main/java/com/laundry_m/mvc/view/SuccessView.java
@@ -0,0 +1,9 @@
+package com.laundry_m.mvc.view;
+
+public class SuccessView {
+	
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+
+}
